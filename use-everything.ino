556db8babcb969042946e446828d9b734309a896
fahmi ahmad <fahmiahmad@tutanota.com>
2025-11-14 21:08:50 +0700

    button interrupt

--- use-everything.ino
+++ use-everything.ino
@@ -4,15 +4,33 @@
 #include "sensor_data.h"
 
 const int BUTTON_PIN = 2;
-int currentMode = 0;
-// temp == 1
-// humidity == 2
-// proximity == 3
-const int NUM_MODES = 3; 
+volatile int currentMode = 0;
+// temp == 0
+// humidity == 1
+// proximity == 2
+const int NUM_MODES = 3;
+
+// debouncing variables
+volatile unsigned long lastInterruptTime = 0;
+const unsigned long debounceDelay = 200; 
+
+// interrupt service routine for button press
+void buttonISR() {
+  unsigned long interruptTime = millis();
+  
+  // debounce: ignore if interrupt triggered within debounceDelay
+  if (interruptTime - lastInterruptTime > debounceDelay) {
+    currentMode = (currentMode + 1) % NUM_MODES;
+    lastInterruptTime = interruptTime;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // attach interrupt on falling edge (button press)
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, FALLING);
 
   // init sensors, display, ble
   initSensors();
@@ -22,20 +40,13 @@ void setup() {
 }
 
 void loop() {
-  static bool lastButtonState = HIGH;
-  bool buttonState = digitalRead(BUTTON_PIN);
-
-  if (buttonState == LOW && lastButtonState == HIGH) {
-    currentMode = (currentMode + 1) % NUM_MODES;
-    delay(200);
-  }
-  lastButtonState = buttonState;
-
   // read sensor data
   getTemperature();
   getHumidity();
   getProximity();
 
-  // Update display with current mode
+  // update display with current mode
   updateDisplay(currentMode);
+  
+  delay(100);
 }
